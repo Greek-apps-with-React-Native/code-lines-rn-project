c562707bbf24921f475cbae0fc69f8a44f668ed8
Fotios Tsakiris <footios76@gmail.com>
2021-10-10 17:51:37 +0300

    basic structure of chat screen

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { FontAwesome } from "@expo/vector-icons";
+import IOSButton from '../components/IOSButton';
 
 const ChatScreen = ({ chat, chatTitle }: { chat: Function, chatTitle: string }) => {
+  const [comment, setComment] = React.useState('');
+
   const showChatList = () => {
     chat();
   };
+
+  const handleCommentChange = () => {
+
+  }
+  const commentHanlder = () => {
+
+  }
   return (
     <View style={styles.chatContainer}>
       <View style={styles.arrowTitleContainer} >
@@ -13,6 +23,34 @@ const ChatScreen = ({ chat, chatTitle }: { chat: Function, chatTitle: string }) 
           <FontAwesome name="arrow-left" size={30} color="cornflowerblue" />
         </TouchableOpacity>
         <Text style={styles.chatTitle} >{chatTitle}</Text>
+      </View>
+      <View style={styles.chatBody} >
+        <View style={styles.chatHeader} >
+          <Text>Header</Text>
+        </View>
+        <View style={styles.commentsContainer} >
+          <Text>Comments</Text>
+        </View>
+        <View style={styles.commentForm} >
+          <Text>commentForm</Text>
+          <TextInput
+            autoFocus
+            autoCapitalize='none'
+            style={styles.input}
+            value={comment}
+            onChangeText={handleCommentChange}
+            placeholder='Write your comment'
+          />
+          <IOSButton
+            title="Send"
+            color='cornflowerblue'
+            onPress={commentHanlder}
+            style={styles.commentButton}
+            positionStyle={styles.commentButtonContainer}
+            disabled={false}
+          />
+        </View>
+
       </View>
     </View>
   );
@@ -27,6 +65,12 @@ const styles = StyleSheet.create({
     marginTop: 40,
     marginRight: 45,
   },
+  chatBody: {
+
+  },
+  chatHeader: {
+
+  },
   chatTitle: {
     fontFamily: "EuphemiaUCAS-Bold",
     fontSize: 25,
@@ -34,7 +78,27 @@ const styles = StyleSheet.create({
     marginLeft: 15,
   },
   chatContainer: {
-    // marginTop: 10,
+    flex: 1
+  },
+  commentButton: {
+
+  },
+  commentButtonContainer: {
+
+  },
+  commentForm: {
+
+  },
+  commentsContainer: {
+
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    fontSize: 30,
+    fontFamily: "Cochin-Bold",
+    width: 300,
+    marginBottom: 50,
   },
 });
 
